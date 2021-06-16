46066fa5a74457054ce0eaa5c94bf1a0dc29d697
Balbastrex <balbastrex@gmail.com>
2021-06-16 19:18:03 +0200

    first activity modified

--- DOM/Examen recuperación 1ev/assets/js/main.js
+++ DOM/Examen recuperación 1ev/assets/js/main.js
@@ -14,27 +14,57 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
-    function buttonstyle(button){
-        button.style.backgroundColor = 'blue';
-        button.style.padding = '1em';
-    }
-    const capa=document.getElementById('buttons-container');
-    const boton1=document.createElement('button'); 
-    const text=document.getElementById('text');
+    // function buttonstyle(button){
+    //     button.style.backgroundColor = 'blue';
+    //     button.style.padding = '1em';
+    // }
+
+
+
+    // const capa=document.getElementById('buttons-container');
+    // const boton1=document.createElement('button'); 
+
+    // const text=document.getElementById('text');
+    // console.log(text);
+
+    // const div2=document.createElement('div');
+    // text.appendChild(div2);
+    // const span1=document.createElement('span');
+    // div2.appendChild(span1);
+
+    const capa = document.getElementById('buttons-container');
+
+    const button1=document.createElement('button');
+    const button2=document.createElement('button');
+    button1.innerText='Contar Palabras'; 
+    button2.innerText='Restablecer'; 
+    button2.disabled = true;
+    capa.appendChild(button1);
+    capa.appendChild(button2);
+
+
+    const textContainer = document.getElementsByClassName('text-container');
+    const text = textContainer[0];
     console.log(text);
+    
     const div2=document.createElement('div');
-    text.appendChild(div2);
     const span1=document.createElement('span');
+    span1.style.fontSize = '200%';
     div2.appendChild(span1);
+    text.appendChild(div2);
 
-
-    capa.appendChild(boton1);
-    boton1.innerText='Contar Palabras'; 
-    boton1.addEventListener('click',()=>{
-      text.textContent = countWord(text);
-      buttonstyle();
+    button1.addEventListener('click',()=>{
+    span1.textContent = countWord(text);
+    div2.appendChild(span1);
+    button2.disabled = false;
     });
 
+    button2.addEventListener('click', ()=>{
+        div2.removeChild(span1);
+        button2.disabled = true;
+    });
+
+    // ejercicio 4
 
 
 });
