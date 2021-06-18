cafd178b1d56347f3e62c7e560437bb87c599cfd
Balbastrex <balbastrex@gmail.com>
2021-06-18 06:34:19 +0200

    exercises 1, 2, 3, 5 & 7 done!

--- Angular/Ex2ev/src/app/app.component.ts
+++ Angular/Ex2ev/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { EXButtonComponent } from './exbutton/exbutton.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Ex2ev';
+export class AppComponent implements OnInit, AfterViewInit{
+  
+  @ViewChild ('exButton') button: EXButtonComponent = new EXButtonComponent;
+
+  h2Title: string = 'Welcome to Web Development!';
+
+  ngOnInit(): void {
+    // this.title = 'Welcome to Web Development!';
+  }
+
+  ngAfterViewInit(): void{
+    this.h2Title = 'Already in!';
+    console.log('Change for exercise 3, done');
+
+    this.button.justDoIt();
+  }
+
+
+
+  
 }
--- Angular/Ex2ev/src/app/exbutton/exbutton.component.ts
+++ Angular/Ex2ev/src/app/exbutton/exbutton.component.ts
@@ -1,10 +1,29 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-exbutton',
   templateUrl: './exbutton.component.html',
   styleUrls: ['./exbutton.component.css']
 })
-export class EXButtonComponent {
+export class EXButtonComponent implements OnInit, AfterViewInit{
 
+  @Input() label: string = 'Click me!';
+
+  justDoIt = () => {
+    console.log('Exercise 5 done!')
+  }
+
+  ngOnInit(): void {
+
+    // this.label = 'Listo para hacer clic';
+  }
+
+  ngAfterViewInit():void {
+    // this.label = 'Listo para hacer clic';
+  }
+
+  labelChanger = () => {
+    this.label = 'Listo para hacer clic';
+    // console.log('button has been pressed');
+  }
 }
